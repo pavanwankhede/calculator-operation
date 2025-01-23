d71bba0b6302e6889b099e11c1d1e2fd110281c2
pavanwankhede <pavanbwankhede@gmail.com>
2025-01-23 14:56:04 +0530

    addd

--- CalculatorOperation/src/com/org/Calculator.java
+++ CalculatorOperation/src/com/org/Calculator.java
@@ -31,15 +31,7 @@ public class Calculator {
 			System.out.println(v*w);
 			}
 		
-		
-		  public static void main(String args[]){
-		  
-		  Calculator cal = new Calculator(); 
-			  cal.add(); 
-			  cal.sub(); 
-			  cal.mul();
-		       cal.div(); }
-		 
+	
 		
 }
 
