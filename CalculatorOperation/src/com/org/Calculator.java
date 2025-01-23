eb10b22efd106922778517c8035afa6d79b486fb
pavanwankhede <pavanbwankhede@gmail.com>
2025-01-23 14:49:19 +0530

    add project and add some perform operations by TL

---
+++ CalculatorOperation/src/com/org/Calculator.java
@@ -0,0 +1,24 @@
+package com.org;
+
+public class Calculator {
+  
+	public void add(){
+
+		int i=50;
+		float y=30.5f;
+		 System.out.println(i+y);
+		}
+		public void sub(){
+		int a=50;
+		int b=15;
+		System.out.println(a-b);
+
+		}
+
+		/*
+		 * public static void main(String args[]){ Calculator cal = new Calculator();
+		 * cal.add(); cal.sub(); cal.mul(); cal.div(); }
+		 */
+		
+}
+
